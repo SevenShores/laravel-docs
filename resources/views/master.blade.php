372bee4fda361b2b3b7ef835ef6946697e12313c
fungku <fungku@gmail.com>
2015-08-28 12:07:52 -0700

    add highlight.js

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -27,6 +27,7 @@
 
 <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
 <script src="/js/app.js'"></script>
 
 @yield('scripts')
